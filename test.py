7fa5c450a502d26e86756146b1c2000d70656ca8
lufei <very90@foxmail.com>
2019-09-21 19:21:05 +0800

    Only for syncing codes.

--- test.py
+++ test.py
@@ -6,19 +6,22 @@
 import time
 
 class Test():
-  '''test class'''
-  name = ''
-  start_time = ''
-  end_time = ''
-  testcase = None
-  host = None
-
-  def start(self):
-    self.start_time = time.time()
-  def end(self):
-    self.end_time = time.time()
-  def get_result(self):
-    pass
+    '''test class'''
+    name = ''
+    start_time = ''
+    end_time = ''
+    testcase = None
+    host = None
+  
+    @property
+    def start(self):
+        self.start_time = time.time()
+    @start.setter
+    def start(self):
+    def end(self):
+        self.end_time = time.time()
+    def get_result(self):
+        pass
 
 
 class Testcase():

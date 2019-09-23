223ac53c9e1f946791e900a15814361c3f942870
lufei <very90@foxmail.com>
2019-09-23 11:38:39 +0800

    starting designing of test engine

---
+++ design.py
@@ -0,0 +1,28 @@
+#!/bin/env python36
+# filename: design.py
+# Author: lufei
+# Date: 20190923 11:31:30
+
+
+
+'''design script for ssautotest'''
+
+# design of test engine
+
+class Test():
+  '''A test, connect testcase with test client; 
+     schedule test run, execute setup before test and clean after test;
+  '''
+
+class Testcase():
+    '''container for testcase, basic case methods'''
+
+class Host():
+    '''container for test clients, basic attributes'''
+
+
+
+
+classes = [
+
+]
